69ba34020151ad0a2662d122286276481a093002
suji <borra.sujani@gmail.com>
2023-06-13 09:48:27 -0400

    docdb and rds

---
+++ vars.tf
@@ -0,0 +1,17 @@
+variable "tags" {}
+variable "env" {}
+variable "subnets" {}
+variable "name" {
+  default = "rds"
+}
+variable "vpc_id" {}
+variable "allow_db_cidr" {}
+variable "engine_version" {}
+variable "kms_arn" {}
+variable "port_no" {
+  default = 3306
+}
+variable "instance_class" {}
+variable "instance_count" {}
+
+
